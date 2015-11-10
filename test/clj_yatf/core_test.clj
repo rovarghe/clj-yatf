bb337fcf0fbfed70cd4dd74360e76fa01c131d50
Roy Varghese <rovarghe@gmail.com>
2015-11-10 13:09:13 -0800

    Fix CCE

--- test/clj_yatf/core_test.clj
+++ test/clj_yatf/core_test.clj
@@ -1,11 +1,7 @@
 (ns clj-yatf.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.pprint]
+            [clojure.test :refer :all]
             [clj-yatf.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 
 (defyat create-tables :version "0.0")
 (defyat create-tables :version "1.0")
